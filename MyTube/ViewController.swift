1ff0c1a3ac711f76f64020570a17115800b0b3d7
Sandeep Shabd <sandeepshabd@gmail.com>
2019-03-17 10:56:33 -0400

    adding code to blue cell rendering

--- MyTube/ViewController.swift
+++ MyTube/ViewController.swift
@@ -15,8 +15,18 @@ class MainViewController: UICollectionViewController {
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.title = "MyTube"
         collectionView?.backgroundColor = UIColor.lightGray
+        collectionView?.register(UICollectionViewCell.self,
+                                 forCellWithReuseIdentifier:  "cellID")
     }
 
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        cell.backgroundColor = UIColor.blue
+        return cell
+    }
 }
 
