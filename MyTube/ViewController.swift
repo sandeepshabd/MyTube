61feb6cd70b1db140fe5fd1c34077e8b1b3d5212
Sandeep Shabd <sandeepshabd@gmail.com>
2019-03-16 20:34:21 -0400

    changing to view Controller

--- MyTube/ViewController.swift
+++ MyTube/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "MyTube"
+        collectionView?.backgroundColor = UIColor.lightGray
     }
 
 
